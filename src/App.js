3e1c75585f13b370ff6b1b8e5514448c95cfb76a
brentleejohnson <brentleejohnson73@gmail.com>
2022-02-03 16:06:27 +0200

    Update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  console.log(React.version);
+
   return (
     <div className="App">
       <header className="App-header">
